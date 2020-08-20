6e9575da07ca54ce21c772e7e66d4f6c64ff09f6
swd762 <ilyakazalin@yandex.ru>
2020-08-20 22:57:50 +0400

    some routes

--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+Route::prefix('auth')->group(function(){
+   Route::post('register', 'AuthController@register');
+   Route::post('login', 'AuthController@login');
+   Route::get('refresh', 'AuthController@refresh');
+
+   Route::group(['middleware'=>'auth:api'], function() {
+      Route::get('user', 'AuthController@user');
+      Route::post('logout', 'AuthController@logout');
+   });
 });
+
+
 
 Route::group(['middleware'=>'auth:api'], function(){
     // Users
